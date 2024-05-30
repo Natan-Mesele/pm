b3c8a5a9bb4e4612b9e32db569876ac59565ec47
Natan-Mesele <huluale12@gmail.com>
2024-05-30 23:44:29 +0300

    jwt

---
+++ pmsb/src/main/java/com/example/project/pmsb/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.example.project.pmsb.config;
+
+public class JwtConstant {
+
+    public static final String SECRETE_KEY="dfghjjjjttaffddfffffdddf";
+
+    public  static final String JWT_HEADER="Authorization";
+}
