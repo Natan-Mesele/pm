b009edba4167a936690d550e0d3e07590c3b0f60
Natan-Mesele <huluale12@gmail.com>
2024-05-30 21:00:47 +0300

    first commit

---
+++ pmsb/src/main/java/com/example/project/pmsb/PmsbApplication.java
@@ -0,0 +1,13 @@
+package com.example.project.pmsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmsbApplication.class, args);
+	}
+
+}
